71a9ee7c93a481b889fab6a4b69fa0edcf040cf3
cx-333 <1354123040@qq.com>
2023-03-31 09:21:05 +0800

    111

---
+++ model.py
@@ -0,0 +1,17 @@
+# _*_ coding: utf-8 _*_
+
+'''
+@Time : 2023/3/30 21:03
+@Author : Xiao Chen
+@File : model.py
+'''
+
+import torch
+from torch import nn
+
+
+class AutoEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
---
+++ parse_args.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+
+"""
+@Time : 2023/3/30 21:11
+@Author : Xiao Chen
+@File : parse_args.py
+"""
+
+import argparse
+
+
+def parse_arguments():
+    args = argparse.ArgumentParser(description="Setting of model parameters and Dataset.")
